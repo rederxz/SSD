a8748bbf8e18cf1d652263ab924831c914083f4e
Rederxz <1148002188@qq.com>
2021-06-25 11:12:13 +0800

    Add model, loss and data

---
+++ Model.py
@@ -0,0 +1,116 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPool2D
+from tensorflow.keras.applications import VGG16
+  
+def SSD300():
+  
+  def base_conv(x):
+    for i in range(1, 22):
+      vgg_layer = vgg.get_layer(index=i)
+      if 'conv' in vgg_layer.name:
+        x = Conv2D(vgg_layer.filters,
+                      vgg_layer.kernel_size, 
+                      vgg_layer.strides,
+                      vgg_layer.padding,
+                      activation=vgg_layer.activation,
+                      name=vgg_layer.name)(x)
+        if vgg_layer.name == 'block4_conv3':
+          fm4_3 = x
+      elif 'pool' in vgg_layer.name: 
+        if '3' in vgg_layer.name: # padding='same' to get 38x38
+          x = MaxPool2D((2, 2), (2, 2), 'same', name=vgg_layer.name)(x)
+        elif '5' in vgg_layer.name: # block5_pool from 2x2_2x2 to 3x3_1x1
+          x = MaxPool2D((3, 3), (1, 1), 'same', name=vgg_layer.name)(x)
+        else:
+          x = MaxPool2D(vgg_layer.pool_size,
+              vgg_layer.strides,
+              vgg_layer.padding, # notion: in vggnet, all pooling layers' paddign == 'valid' , is it ok?
+              name=vgg_layer.name)(x)
+      elif 'fc' in vgg_layer.name:
+        if vgg_layer.name == 'fc1':
+          x = Conv2D(1024, (3, 3), (1, 1), 'same', 
+                        dilation_rate=(6, 6),
+                        activation=vgg_layer.activation,
+                        name=vgg_layer.name)(x) # decimate (6, 6) because block5_pool do not downsample
+        elif vgg_layer.name == 'fc2':
+          x = Conv2D(1024, (1, 1), (1, 1), 'same',
+                        activation=vgg_layer.activation,
+                        name=vgg_layer.name)(x)
+          fm7 = x
+    
+    return [fm4_3, fm7]
+  
+  def aux_conv(x):
+    x = Conv2D(256, (1, 1), activation='relu')(x)
+    fm8_2 = Conv2D(512, (3, 3), (2, 2), 'same', activation='relu', name='block8_conv2')(x)
+    x = Conv2D(128, (1, 1), activation='relu')(fm8_2)
+    fm9_2 = Conv2D(256, (3, 3), (2, 2), 'same', activation='relu', name='block9_conv2')(x)
+    x = Conv2D(128, (1, 1), activation='relu')(fm9_2)
+    fm10_2 = Conv2D(256, (3, 3), activation='relu', name='block10_conv2')(x)
+    x = Conv2D(128, (1, 1), activation='relu')(fm10_2)
+    fm11_2 = Conv2D(256, (3, 3), activation='relu', name='block11_conv2')(x)
+    return [fm8_2, fm9_2, fm10_2, fm11_2]
+  
+  def pred_conv(x, priors_per_tile, fm_size):
+    loc_output = []
+    cls_output = []
+    for i in range(len(fms)):
+      loc_tmp = Conv2D(priors_per_tile[i]*4, (3, 3), padding='same')(fms[i]) # H * W * 25
+      loc_tmp = tf.reshape(loc_tmp, [-1, fm_size[i]*priors_per_tile[i], 4]) # reshape
+      loc_output.append(loc_tmp)
+      cls_tmp = Conv2D(priors_per_tile[i]*21, (3, 3), padding='same')(fms[i])
+      cls_tmp = tf.reshape(cls_tmp, [-1, fm_size[i]*priors_per_tile[i], 21])
+      cls_output.append(cls_tmp)
+    loc_output = tf.concat(loc_output, 1)
+    cls_output = tf.concat(cls_output, 1)
+
+    return [loc_output, cls_output]
+
+  def load_weight():
+    # load weights from vggnet
+    for i in range(1, 21):
+      base_layer = model.get_layer(index=i)
+      vgg_layer = vgg.get_layer(base_layer.name)
+      if 'conv' in vgg_layer.name:
+        base_layer.set_weights(vgg_layer.get_weights())
+      elif 'fc' in vgg_layer.name:
+        # print(vgg_layer.name)
+        if vgg_layer.name == 'fc1':
+          weights = vgg_layer.get_weights()
+          weights[0] = weights[0].reshape([7, 7, 512, 4096])
+          weights[0] = weights[0][::3, ::3, :, ::4] # decimate the weights
+          weights[1] = weights[1][::4]
+          base_layer.set_weights(weights)
+        elif vgg_layer.name == 'fc2':
+          weights = vgg_layer.get_weights()
+          weights[0] = weights[0].reshape([1, 1, 4096, 4096])
+          weights[0] = weights[0][:, :, ::4, ::4] # decimate the weights
+          weights[1] = weights[1][::4]
+          base_layer.set_weights(weights)
+
+  vgg = VGG16()
+
+  input = Input((300, 300, 3))
+
+  # base conv
+  fm4_3, fm7 = base_conv(input)
+
+  # aux conv
+  fms = aux_conv(fm7)
+
+  # pred conv
+  fms = [fm4_3, fm7] + fms
+  priors_per_tile = [4, 6, 6, 6, 4, 4]
+  fm_size = [38*38, 19*19, 10*10, 5*5, 3*3, 1*1]
+  loc_output, cls_output = pred_conv(fms, priors_per_tile, fm_size)
+
+  model = Model(input, [loc_output, cls_output])
+
+  load_weight()
+
+  return model
+
+
+model = SSD300()
+model.summary()
---
+++ VOC.py
@@ -0,0 +1,268 @@
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import numpy as np
+import math
+
+
+def load_voc_dataset(sub=True):
+    if sub:
+        ds_train = tfds.load('voc/2007', split='train+validation', shuffle_files=True)
+        ds_val = tfds.load('voc/2007', split='test')
+    else:
+        ds_train_a = tfds.load('voc/2007', split='train+validation', shuffle_files=True)
+        ds_train_b = tfds.load('voc/2012', split='train+validation', shuffle_files=True)
+        ds_train = ds_train_b.concat(ds_train_a)
+        ds_val = tfds.load('voc/2007', split='test')
+
+    return ds_train, ds_val
+
+'''TODO:
+    add data augmentation
+'''
+def prepare():
+    '''decode elems in the original dataset and do match
+    args:
+        the original dataset
+    returns:
+        a new dataset, each elem is a pair of image and targets
+    '''
+    
+
+
+'''TODO:
+    1. check where to add the 'bgd' class - might be in the match function
+    
+'''
+
+
+def decode(elem):
+    ''' to decode an element from voc dataset provided by tfds
+    args:
+        elem
+            an element from voc dataset provided by tfds
+    returns:
+        image 
+            tf.Tensor, the image of the elem
+        gt
+            a dict, gt['label'] == the label of objects,
+            gt['bbox'] == the bouding box of objects, in the
+            format of center-size coords
+    '''
+    image = elem['image']
+    gts = {
+        'bbox': elem['object']['bbox'],
+        'label': elem['object']['label']
+    }
+
+    return image, gts
+
+
+def cc2bc(cc):
+    '''convert a group of center-size coords to boundary coords'''
+    x_min = cc[:, 0] - cc[:, 2] / 2.
+    x_max = x_min + cc[:, 2]
+    y_min = cc[:, 1] - cc[:, 3] / 2.
+    y_max = y_min + cc[:, 3]
+    
+    return tf.stack([x_min, y_min, x_max, y_max], -1)
+
+def bc2cc(bc):
+    '''convert a group of coords to center-size coords'''
+    x_c = (bc[:, 2] - bc[:, 0]) / 2.
+    y_c = (bc[:, 3] - bc[:, 1]) / 2.
+    w = bc[:, 2] - bc[:, 0]
+    h = bc[:, 3] - bc[:, 1]
+    
+    return tf.stack([x_c, y_c, w, h], -1)
+
+def cal_iou(bboxes_a, bboxes_b):
+    '''calculate iou betwwen two groups of bboxes
+    args:
+        bboxes_a
+            a numpy array of bbox coords (with boundary coords)
+        bboxes_b
+            a numpy array of bbox coords (with boundary coords)
+        the two array should have the same shape (None, 4)
+    returns:
+        a numpy array of shape (None, 1)
+    '''
+
+    max_min = tf.min(bboxes_a[:, 2:], bboxes_b[:, 2:])
+    min_max = tf.max(bboxes_a[:, :2], bboxes_b[:, :2])
+
+    mul = (max_min - min_max)
+    mul = mul * (mul > 0)
+    inter = mul[:, 0] * mul[:, 1]
+
+    union = (bboxes_a[:, 2] - bboxes_a[:, 0]) * (bboxes_a[:, 3] - bboxes_a[:, 1]) + \
+            (bboxes_b[:, 2] - bboxes_b[:, 0]) * (bboxes_b[:, 3] - bboxes_b[:, 1]) - inter
+
+    iou = inter / union
+    
+    return iou
+
+def cal_offset(d_bboxes, g_bboxes):
+    '''calculate offset from d_bboxes to g_bboxes
+    args:
+        bboxes_a
+            a numpy array of bbox coords (with center-size coords)
+        bboxes_b
+            a numpy array of bbox coords (with center-size coords)
+        the two array should have the same shape (None, 4)
+    returns:
+        a numpy array of shape (None, 4)
+    '''
+
+    c_x_offsets = (g_bboxes[:, 0] - d_bboxes[:, 0]) / d_bboxes[:, 0]
+    c_y_offsets = (g_bboxes[:, 1] - d_bboxes[:, 1]) / d_bboxes[:, 1]
+    w_offsets = tf.math.log(g_bboxes[:, 2] / d_bboxes[:, 2])
+    h_offsets = tf.math.log(g_bboxes[:, 3] / d_bboxes[:, 3])
+
+    return tf.stack([c_x_offsets, c_y_offsets, w_offsets, h_offsets], -1)
+
+def match(anchor_bboxes, gts):
+    '''map anchors to gts
+    args:
+        anchor_bbox
+            a list of tf.constant, as defined in the SSDAnchorGenerator
+        gts
+            a dict, as defined in the decode function
+    returns:
+        targets
+            a list of target,  for each target
+            target['label'] is the target class
+            target['offset'] is [g_c_x, g_c_y, g_w, g_h]
+    '''
+    gt_bboxes = gts['bbox']
+    gt_labels = gts['label']
+
+    ious = []
+    num_of_anchors = anchor_bboxes.shape[0]
+
+    # TODO: tile and reshape
+    for gt_bbox in gt_bboxes:
+        repeated_gt_bbox = np.tile(gt_bbox, (num_of_anchors, 1))
+        cur_ious = cal_iou(repeated_gt_bbox, anchor_bboxes)
+        ious.append(cur_ious)
+    
+    ious = tf.stack(ious, -1)
+
+    # two rules to do the match depending on ious
+    # 1. anchor-wise 2. gt-wise
+
+    labels = []
+    target_bboxes = []
+    
+    # anchor-wise
+    max_iou_gt_idxs = tf.math.argmax(ious, axis=-1)
+    target_labels = tf.gather(gt_labels, max_iou_gt_idxs)
+    target_bboxes = tf.gather(gt_bboxes, max_iou_gt_idxs)
+
+    # up to now, all anchors should have a label and a target bbox
+    
+    # gt-wise
+    max_iou_anchor_idxs = tf.math.argmax(ious, axis=0)
+    target_labels = tf.gather(gt_labels, max_iou_gt_idxs)
+    target_bboxes = tf.gather(gt_bboxes, max_iou_gt_idxs)
+
+    for i, gt in enumerate(gts):
+        max_iou_anchor_idx = np.argmax(ious[:, i])
+        if max_iou_anchor_idx <= 0.5:
+            continue
+        labels[max_iou_anchor_idx] = gt['label']
+        target_bboxes[max_iou_anchor_idx] = gt['bbox']
+    target_bboxes = np.array(target_bboxes)
+
+    # turn the coords to center-size form
+    anchor_bboxes = bc2cc(anchor_bboxes)
+    target_bboxes = bc2cc(target_bboxes)
+
+    offsets = cal_offset(anchor_bboxes, target_bboxes)
+
+    # now we have tow lists, labels and offsets, 
+    # the order is the same as the corresponding anchors'
+
+    targets = []
+    for label, offset in zip(labels, offsets):
+        targets.append(
+            {
+                'label':label,
+                'offset':offset
+            }
+        )
+    
+    return targets
+
+'''TODO:
+    1. make sure the order of each anchor whether correspond
+    to the order of flattened feature map, for func
+        make_anchors_for_one_fm
+        make_anchors_for_multi_fm
+    2. for SSDAnchorGenerator, in case we want to change the default
+    settings
+'''
+class SSDAnchorGenerator:
+    ''' generate anchors defined in SSD'''
+    
+    def __init__(self, default=True):
+        if default:
+            # for each feature map
+            self.fm_scales = [0.1, 0.2, 0.375, 0.55, 0.725, 0.9]
+            self.fm_sizes = [(38, 38), (19, 19), (10, 10), (5, 5), (3, 3), (1, 1)]
+            self.fm_subset = [True, False, False, False, True, True]
+
+            # for each tile in a feature map
+            self.bbox_aspects = [1., 2., 0.5, 3., 1./3.]
+        else:
+            pass    
+
+    def make_anchors_for_one_fm(self, size, scale, extra_scale, subset):
+        ''' make anchors for a particular feature map
+        args:
+            size
+                tuple (H, W), hight and width of the feature map
+            scale
+                float, the scale of each anchor, the area in the image is scale * scale
+            extra_scale
+                float, the extra scale for each anchor with aspect == 1:1
+            subset
+                bool, whether to use the simple aspects
+        returns:
+            a list of tf.constant
+                each elem is the coords of the anchor,  anchors are 
+                in the order of different setting, width, height
+        '''
+        anchors = []
+
+         # width and height wrt each aspect
+        aspects = self.bbox_aspects[:3] if subset else self.bbox_aspects
+        w = [scale * math.sqrt(a) for a in aspects] + [extra_scale]
+        h = [scale / math.sqrt(a) for a in aspects] + [extra_scale]
+        w_h_pairs = zip(w, h)
+
+        # the order matters, 'i' and 'j' (tiles in the feature map) must be iterated after the aspects,
+        # to ensure that neighbour anchors have the same aspect
+        for w_h in w_h_pairs:
+            for j in range(size[0]): # size is in the order of (H, W), height direction
+                for i in range(size[1]): # size is in the order of (H, W), width direction
+                    c_x_y = [(i + 0.5) / size[1], (j + 0.5) / size[0]] 
+                    anchors.append(tf.constant(c_x_y + list(w_h))) # [x_c, y_c, w, h]
+        return anchors
+
+    def make_anchors_for_multi_fm(self):
+        '''make anchors for multiple feature maps in different stages
+        returns:
+            a list of dict
+                each dict['bbox'] is the coords of the anchor, in the order of different stage, setting, 
+                width, height
+        '''
+        anchors = []
+
+        # add 1.0 in the end to calculate extra scales
+        scales = self.fm_scales + [1.0]
+
+        for i in range(len(self.fm_scales)):
+            extra_scale = math.sqrt(scales[i] * scales[i+1]) # the extra scale for each stage of feature map
+            anchors += self.make_anchors_for_one_fm(self.fm_sizes[i], self.fm_scales[i], extra_scale, self.fm_subset[i])
+
+        return anchors
