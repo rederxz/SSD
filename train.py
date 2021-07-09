34524731c654fa949f95c4b2f9c167ce51fda1ee
Rederxz <1148002188@qq.com>
2021-07-09 10:57:01 +0800

    change

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
+import os
+
+import tensorflow as tf
 from tensorflow.keras.optimizers.schedules import PiecewiseConstantDecay
-from tensorflow.keras.callbacks import LearningRateScheduler
 from tensorflow.keras.optimizers import SGD
 
 from voc import load_voc_dataset, prepare
@@ -11,6 +13,19 @@ from loss import SSDLoss
 # TODO: add mAP
 
 
+def setup_tpu():
+    tpu_address = os.environ.get("COLAB_TPU_ADDR")
+    if tpu_address:
+        tpu_address = "grpc://" + tpu_address
+        tf.keras.backend.clear_session()
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver(tpu_address)
+        tf.config.experimental_connect_to_cluster(tpu)
+        tf.tpu.experimental.initialize_tpu_system(tpu)
+        strategy = tf.distribute.TPUStrategy(tpu)
+        return strategy
+
+strategy = setup_tpu()
+
 BATCH_SIZE = 32
 EPOCH = 232  # iterations per epoch = 5011 / BATCH_SIZE
 
@@ -18,10 +33,6 @@ EPOCH = 232  # iterations per epoch = 5011 / BATCH_SIZE
 ds_train, ds_test = load_voc_dataset()
 ds_train = prepare(ds_train, batch_size=BATCH_SIZE, training=True)
 ds_test = prepare(ds_test, batch_size=128)
-
-# model, loss, optimizer, metric
-model = SSD()
-model.build((None, 300, 300, 3))
 
 boundaries = [154, 193]
 values = [1e-3, 1e-4, 1e-5]
@@ -31,7 +42,10 @@ optimizer = SGD(learning_rate=lr_schedule, momentum=0.9)
 # metrics = [SSDLoss, mAP]
 metrics = [SSDLoss, ]
 
-model.compile(loss=SSDLoss, optimizer=optimizer, metrics=metrics)
+with strategy.scope():
+    model = SSD()
+    model.build((None, 300, 300, 3))
+    model.compile(loss=SSDLoss, optimizer=optimizer, metrics=metrics)
 
 # train
 model.fit(ds_train, validation_data=ds_test, epochs=EPOCH)
